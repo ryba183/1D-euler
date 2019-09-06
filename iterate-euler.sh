caf66b6e6b8ab2796bbbfd039a002de8e946a968
Stephen Richer <sr467@bath.ac.uk>
2019-09-06 21:12:28 +0100

    Updated bin range.

--- iterate-euler.sh
+++ iterate-euler.sh
@@ -6,8 +6,8 @@ function iterate {
     ncells=$1
     mid=$(( (ncells / 2) + 1))
     # Run algorithm - store time and midpoint in var
-    var=$( TIMEFORMAT="%R"; { time ./euler_equations_commandline -n "${ncells}" -t 0.15 -d 1 -D 1 -v -2 -V 2 -p 0.4 -P 0.4 | cut -f 2 | sed -n "$mid p"; } 2>&1 )
-  
+    var=$( TIMEFORMAT="%U"; { time ./1d_euler -n "${ncells}" -t 0.15 -d 1 -D 1 -v -2 -V 2 -p 0.4 -P 0.4 | cut -f 2 | sed -n "$mid p"; } 2>&1 )
+
     velocity=$(echo "${var}" | head -n1)
     time=$(echo "${var}" | tail -n1)
     echo -e ${ncells}"\t"${velocity}"\t"${time}
@@ -15,6 +15,7 @@ function iterate {
 
 export -f iterate
 
-#binrange=( $(seq 101 100 1000) $(seq 1001 1000 10000) $(seq 10001 10000 110000) $(seq 100001 100000 1100000) )
-binrange=( 101 501 1001 5001 10001 50001 100001 500001 1000001 )
-parallel -j 1 iterate ::: "${binrange[@]}"
+binrange=( $(seq 1001 1000 10000) $(seq 10001 10000 100000) $(seq 100001 300000 1000001))
+
+# Set -j to increase thread count for parrallel execution
+parallel -j 6 iterate ::: "${binrange[@]}"
